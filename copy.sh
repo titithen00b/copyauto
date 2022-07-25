987fbdba8b66e68af3ef7d0feb793dfa2a1d2703
Valentin Roche <54205303+titithen00b@users.noreply.github.com>
2022-07-25 17:38:36 +0200

    Create copy.sh

---
+++ copy.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Set de la varibale J-1 avec affichage : YYYYMMJJ
+j_1=`date -d "$(date +%Y-%m-%d) -3 day" +%Y%m%d`
+echo "$j_1"
+
+#Test J-1 pas samedi ni dimanche
+J=`date -d "-1 day" +%A`
+if [ $J == "Sunday" ] || [  $J == "Saturday" ] || [ $J == "dimanche" ] || [ $J == "samedi" ]
+then echo "Aucune sauvegarde le samedi/dimanche" && param=0
+else echo "Let's go !" && param=1
+fi
+
+
+
+for i in $(ls | grep "$j_1") 
+do
+echo test
+done
